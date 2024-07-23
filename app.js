0b7f05abe6185edd50d1030a43a9a18605fc1d04
MohdSohrab <sohrabmohd2003@gmail.com>
2024-07-23 19:35:28 +0530

    change in main branch

--- app.js
+++ app.js
@@ -3,12 +3,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-<<<<<<< HEAD
 // add new features in the web interface
-
-=======
 const routes =3999;
->>>>>>> featured
+
 
 
 // middleware for parsing JSON request bodies
