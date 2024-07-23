a229a37c83b194312f8a988b3202969bd548c08c
MohdSohrab <sohrabmohd2003@gmail.com>
2024-07-23 19:21:32 +0530

    add new

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-// add new features in the web interface lke from http://localhost
+// add new features in the web interface lke form http://localhost
 
 
 // middleware for parsing JSON request bodies
