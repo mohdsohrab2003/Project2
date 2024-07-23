a0531dd20ce0cbc2a391ec5343d1bc2aa6f558a1
MohdSohrab <sohrabmohd2003@gmail.com>
2024-07-23 19:24:20 +0530

    Add routes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-// add new features in the web interface lke form http://localhost
+const routes =3999;
 
 
 // middleware for parsing JSON request bodies
